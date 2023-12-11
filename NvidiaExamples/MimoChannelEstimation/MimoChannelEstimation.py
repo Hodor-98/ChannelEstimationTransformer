197f2b4676f090388f909e25215496cba9520897
B. Hodes <b.hodes@student.tue.nl>
2023-12-11 15:53:41 +0100

    Init commit

---
+++ NvidiaExamples/MimoChannelEstimation/MimoChannelEstimation.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import tensorflow as tf
+import sionna
+
+from tensorflow.keras import Model
+
+from sionna.mimo import StreamManagement
+from sionna.utils import QAMSource, compute_ser, BinarySource, sim_ber, ebnodb2no, QAMSource
+from sionna.mapping import Mapper
+from sionna.ofdm import ResourceGrid, ResourceGridMapper, LSChannelEstimator, LMMSEInterpolator, LinearDetector, KBestDetector, EPDetector, MMSEPICDetector
+from sionna.channel import GenerateOFDMChannel, OFDMChannel, gen_single_sector_topology
+from sionna.channel.tr38901 import UMi, Antenna, PanelArray
+from sionna.fec.ldpc import LDPC5GEncoder
+from sionna.fec.ldpc import LDPC5GDecoder
+
+NUM_OFDM_SYMBOLS = 14
+FFT_SIZE = 12*4 # 4 PRBs
+SUBCARRIER_SPACING = 30e3 # Hz
+CARRIER_FREQUENCY = 3.5e9 # Hz
+SPEED = 30. # m/s
+
+# The user terminals (UTs) are equipped with a single antenna
+# with vertial polarization.
+UT_ANTENNA = Antenna(polarization='single',
+                     polarization_type='V',
+                     antenna_pattern='omni', # Omnidirectional antenna pattern
+                     carrier_frequency=CARRIER_FREQUENCY)
+
+# The base station is equipped with an antenna
+# array of 8 cross-polarized antennas,
+# resulting in a total of 16 antenna elements.
+NUM_RX_ANT = 16
+BS_ARRAY = PanelArray(num_rows_per_panel=4,
+                      num_cols_per_panel=2,
+                      polarization='dual',
+                      polarization_type='cross',
+                      antenna_pattern='38.901', # 3GPP 38.901 antenna pattern
+                      carrier_frequency=CARRIER_FREQUENCY)
+
+# 3GPP UMi channel model is considered
+CHANNEL_MODEL = UMi(carrier_frequency=CARRIER_FREQUENCY,
+                    o2i_model='low',
+                    ut_array=UT_ANTENNA,
+                    bs_array=BS_ARRAY,
+                    direction='uplink',
+                    enable_shadow_fading=False,
+                    enable_pathloss=False)
+
+
+rg = ResourceGrid(num_ofdm_symbols=NUM_OFDM_SYMBOLS,
+                  fft_size=FFT_SIZE,
+                  subcarrier_spacing=SUBCARRIER_SPACING)
+channel_sampler = GenerateOFDMChannel(CHANNEL_MODEL, rg)
+
+
+def sample_channel(batch_size):
+    # Sample random topologies
+    topology = gen_single_sector_topology(batch_size, 1, 'umi', min_ut_velocity=SPEED, max_ut_velocity=SPEED)
+    CHANNEL_MODEL.set_topology(*topology)
+
+    # Sample channel frequency responses
+    # [batch size, 1, num_rx_ant, 1, 1, num_ofdm_symbols, fft_size]
+    h_freq = channel_sampler(batch_size)
+    # [batch size, num_rx_ant, num_ofdm_symbols, fft_size]
+    h_freq = h_freq[:,0,:,0,0]
+
+    return h_freq
+
+
+@tf.function(jit_compile=True) # Use XLA for speed-up
+def estimate_covariance_matrices(num_it, batch_size):
+    freq_cov_mat = tf.zeros([FFT_SIZE, FFT_SIZE], tf.complex64)
+    time_cov_mat = tf.zeros([NUM_OFDM_SYMBOLS, NUM_OFDM_SYMBOLS], tf.complex64)
+    space_cov_mat = tf.zeros([NUM_RX_ANT, NUM_RX_ANT], tf.complex64)
+    for _ in tf.range(num_it):
+        # [batch size, num_rx_ant, num_ofdm_symbols, fft_size]
+        h_samples = sample_channel(batch_size)
+
+        #################################
+        # Estimate frequency covariance
+        #################################
+        # [batch size, num_rx_ant, fft_size, num_ofdm_symbols]
+        h_samples_ = tf.transpose(h_samples, [0,1,3,2])
+        # [batch size, num_rx_ant, fft_size, fft_size]
+        freq_cov_mat_ = tf.matmul(h_samples_, h_samples_, adjoint_b=True)
+        # [fft_size, fft_size]
+        freq_cov_mat_ = tf.reduce_mean(freq_cov_mat_, axis=(0,1))
+        # [fft_size, fft_size]
+        freq_cov_mat += freq_cov_mat_
+
+        ################################
+        # Estimate time covariance
+        ################################
+        # [batch size, num_rx_ant, num_ofdm_symbols, fft_size]
+        time_cov_mat_ = tf.matmul(h_samples, h_samples, adjoint_b=True)
+        # [num_ofdm_symbols, num_ofdm_symbols]
+        time_cov_mat_ = tf.reduce_mean(time_cov_mat_, axis=(0,1))
+        # [num_ofdm_symbols, num_ofdm_symbols]
+        time_cov_mat += time_cov_mat_
+
+        ###############################
+        # Estimate spatial covariance
+        ###############################
+        # [batch size, num_ofdm_symbols, num_rx_ant, fft_size]
+        h_samples_ = tf.transpose(h_samples, [0,2,1,3])
+        # [batch size, num_ofdm_symbols, num_rx_ant, num_rx_ant]
+        space_cov_mat_ = tf.matmul(h_samples_, h_samples_, adjoint_b=True)
+        # [num_rx_ant, num_rx_ant]
+        space_cov_mat_ = tf.reduce_mean(space_cov_mat_, axis=(0,1))
+        # [num_rx_ant, num_rx_ant]
+        space_cov_mat += space_cov_mat_
+
+    freq_cov_mat /= tf.complex(tf.cast(NUM_OFDM_SYMBOLS*num_it, tf.float32), 0.0)
+    time_cov_mat /= tf.complex(tf.cast(FFT_SIZE*num_it, tf.float32), 0.0)
+    space_cov_mat /= tf.complex(tf.cast(FFT_SIZE*num_it, tf.float32), 0.0)
+
+    return freq_cov_mat, time_cov_mat, space_cov_mat
+
+batch_size = 1000
+num_iterations = 100
+
+sionna.Config.xla_compat = True # Enable Sionna's support of XLA
+FREQ_COV_MAT, TIME_COV_MAT, SPACE_COV_MAT = estimate_covariance_matrices(batch_size, num_iterations)
+sionna.Config.xla_compat = False # Disable Sionna's support of XLA
+
+# FREQ_COV_MAT : [fft_size, fft_size]
+# TIME_COV_MAT : [num_ofdm_symbols, num_ofdm_symbols]
+# SPACE_COV_MAT : [num_rx_ant, num_rx_ant]
+
+np.save('freq_cov_mat', FREQ_COV_MAT.numpy())
+np.save('time_cov_mat', TIME_COV_MAT.numpy())
+np.save('space_cov_mat', SPACE_COV_MAT.numpy())
+
+
+# FREQ_COV_MAT = np.load('freq_cov_mat.npy')
+# TIME_COV_MAT = np.load('time_cov_mat.npy')
+# SPACE_COV_MAT = np.load('space_cov_mat.npy')
+
+
+fig, ax = plt.subplots(3,2, figsize=(10,12))
+fig.suptitle("Time and frequency channel covariance matrices")
+
+ax[0,0].set_title("Freq. cov. Real")
+im = ax[0,0].imshow(FREQ_COV_MAT.real, vmin=-0.3, vmax=1.8)
+ax[0,1].set_title("Freq. cov. Imag")
+im = ax[0,1].imshow(FREQ_COV_MAT.imag, vmin=-0.3, vmax=1.8)
+
+ax[1,0].set_title("Time cov. Real")
+im = ax[1,0].imshow(TIME_COV_MAT.real, vmin=-0.3, vmax=1.8)
+ax[1,1].set_title("Time cov. Imag")
+im = ax[1,1].imshow(TIME_COV_MAT.imag, vmin=-0.3, vmax=1.8)
+
+ax[2,0].set_title("Space cov. Real")
+im = ax[2,0].imshow(SPACE_COV_MAT.real, vmin=-0.3, vmax=1.8)
+ax[2,1].set_title("Space cov. Imag")
+im = ax[2,1].imshow(SPACE_COV_MAT.imag, vmin=-0.3, vmax=1.8)
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(im, cax=cbar_ax)
+
+plt.show()
